840b42c54f4dae75dc13539a60a185b5f7cb08b8
s-almeda <shm.almeda@berkeley.edu>
2024-07-08 12:33:03 -0700

    licenses are important

--- public/gesture_recognition_script.js
+++ public/gesture_recognition_script.js
@@ -1,3 +1,18 @@
+
+// adapted from code from here: https://codepen.io/mediapipe-preview/pen/zYamdVd
+// Copyright 2023 The MediaPipe Authors.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//      http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import {
   GestureRecognizer,
   HandLandmarker,
@@ -76,6 +91,8 @@ document.addEventListener("DOMContentLoaded", () => {
       const n = allCanvas[i];
       n.parentNode.removeChild(n);
     }
+
+    //RESULTS FOR GESTURE RECOGNITION 
 
     const results = gestureRecognizer.recognize(event.target);
 
@@ -184,6 +201,8 @@ document.addEventListener("DOMContentLoaded", () => {
     if (video.currentTime !== lastVideoTime) {
       lastVideoTime = video.currentTime;
       results = gestureRecognizer.recognizeForVideo(video, nowInMs);
+
+      //RESULTS FOR HANDLANDMARKS 
       const handLandmarkerResults = await handLandmarker.detectForVideo(video, nowInMs);
 
       canvasCtx.save();
